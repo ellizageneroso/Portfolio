eb418a9ade1a2a87f549eb6a02394071ff98e3f9
ellizageneroso <ellizageneroso@gmail.com>
2026-06-05 23:56:36 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const texts=[
+"IT STUDENT",
+"ASPIRING DEVELOPER",
+"ASPIRING UI/UX DESIGNER"
+];
+
+let count=0;
+let index=0;
+let currentText="";
+let letter="";
+
+(function type(){
+
+if(count===texts.length){
+count=0;
+}
+
+currentText=texts[count];
+letter=currentText.slice(0,++index);
+
+document.querySelector(".typing").textContent=letter;
+
+if(letter.length===currentText.length){
+
+count++;
+index=0;
+
+setTimeout(type,1500);
+}else{
+setTimeout(type,100);
+}
+
+})();
